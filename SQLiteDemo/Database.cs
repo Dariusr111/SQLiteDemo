071f9de2af0dcb288d44ebb1b85e844435771e4b
dariusr <darius.r111@gmail.com>
2023-01-29 18:13:39 +0200

    First commit

---
+++ SQLiteDemo/Database.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SQLiteDemo
+{
+	internal class Database
+	{
+		public SQLiteConnection myConnection;
+
+		//constructor
+		public Database()
+		{
+			myConnection = new SQLiteConnection("Data Source=C:/database/test3.db");
+			if (!File.Exists("C:/database/test3.db"))
+			{
+				SQLiteConnection.CreateFile("C:/database/test3.db");
+				System.Console.WriteLine("Database file created");
+			}
+			else
+			{
+				System.Console.WriteLine("Database file already created");
+			}
+									
+		}
+
+		// checking if db connection open
+
+		public void OpenConnection()
+		{
+			if (myConnection.State != System.Data.ConnectionState.Open)
+			{
+				myConnection.Open();
+			}
+		}
+
+		// checking if db connection closed
+		public void CloseConection()
+		{
+			if (myConnection.State != System.Data.ConnectionState.Closed)
+			{
+				myConnection.Close();
+			}
+		}
+
+	}
+}
---
+++ SQLiteDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteDemo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Database databaseObject = new Database();
+
+			// INSERT INTO DATABASE
+
+			string query = "INSERT INTO users (`username`, `password`, `company`, `email`, `role`, `client_code`) VALUES (@v1, @v2, @v3, @v4, @v5, @v6)";
+			SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+			databaseObject.OpenConnection();
+			myCommand.Parameters.AddWithValue("@v1", "Vardas");
+			myCommand.Parameters.AddWithValue("@v2", "123-pdY");
+			myCommand.Parameters.AddWithValue("@v3", "Salda");
+			myCommand.Parameters.AddWithValue("@v4", "vardas@salda.lt");
+			myCommand.Parameters.AddWithValue("@v5", 1);
+			myCommand.Parameters.AddWithValue("@v6", "TP4");
+			var result = myCommand.ExecuteNonQuery();
+			databaseObject.CloseConection();
+			
+			Console.WriteLine("Rows Added : {0}", result);
+			Console.ReadKey();
+		}
+	}
+}
